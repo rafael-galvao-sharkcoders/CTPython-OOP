d04cbe37f3dfe43f8484f3e80c779e3769a8be0d
rafael-galvao-sharkcoders <149248619+rafael-galvao-sharkcoders@users.noreply.github.com>
2024-04-13 15:28:21 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+import file_menager
+
+
+
+'''
+- Criar sistema de lofin e registo;
+-
+'''
+
+def login(user, password):
+	#login fuction
+	pass
+
+def register(user, password):
+	#register fuction
+	pass
+
+def banner():
+	print("--------------------")
+	print("|Game Of The Filles|")
+	print("--------------------")
+	pass
+
+def menu():
+	print("1 - Login \n2 - Register\n\n> ")
+
+
+def main():
+	banner()
+	menu()
+
+
+if __name__ == "__main__":
+	main()
